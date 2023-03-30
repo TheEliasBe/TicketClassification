817eafc7bba32b65c6ba3725bbcca7e225b4507c
Elias Berger <elias.berger@kertos.io>
2023-03-30 21:13:01 +0200

    stemmer and dropna

--- src/ticketclassification/pipelines/preprocessing/nodes.py
+++ src/ticketclassification/pipelines/preprocessing/nodes.py
@@ -3,7 +3,49 @@ This is a boilerplate pipeline 'preprocessing'
 generated using Kedro 0.18.7
 """
 import pandas as pd
+import numpy as np
+from kedro.framework.cli import catalog
+from nltk.stem import PorterStemmer, Cistem
+stemmer = Cistem()
 
 
-def input(tickets: pd.DataFrame):
+def merge_tickets_with_contents(tickets: pd.DataFrame, contents: pd.DataFrame) -> pd.DataFrame:
+    return pd.merge(contents, tickets, on="ID", how="inner")
+
+
+def reduce_columns(tickets: pd.DataFrame):
+    """
+    Reduce the number of columns to the ones we need
+    :param tickets:
+    :return:
+    """
+    tickets = tickets[["Ticket Label", "Abteilung Label", "Produkt Label", "Text"]]
     return tickets
+
+
+def drop_whitespace(tickets: pd.DataFrame):
+    """
+    Drop all rows with whitespace in the text column
+    :param tickets:
+    :return:
+    """
+    df = tickets.replace(r'^\s*$', np.nan, regex=True)
+    df.dropna(inplace=True)
+    return df
+
+
+def _stem_sentence(sentence):
+    # split the sentence into words (whitespace tokenizer)
+    words = sentence.split()
+    # apply the stemmer to each word
+    stemmed_words = [stemmer.stem(word) for word in words]
+    # join the stemmed words back into a sentence
+    stemmed_sentence = ' '.join(stemmed_words)
+    return stemmed_sentence
+
+
+def porter_stemmer(tickets: pd.DataFrame):
+    tickets["Text"] = tickets["Text"].apply(_stem_sentence)
+    return tickets
+
+
--- src/ticketclassification/pipelines/preprocessing/pipeline.py
+++ src/ticketclassification/pipelines/preprocessing/pipeline.py
@@ -5,15 +5,28 @@ generated using Kedro 0.18.7
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import input
+from .nodes import merge_tickets_with_contents, reduce_columns, drop_whitespace, porter_stemmer
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
-            func=input,
-            inputs="tickets_2022",
-            outputs="number",
-            name="hello_world",
-        )
+            func=merge_tickets_with_contents,
+            inputs=["tickets_2022", "contents_2022"],
+            outputs="merged_df",
+            name="merge_tickets_with_contents",
+        ),
+        node(
+            func=reduce_columns,
+            inputs="merged_df",
+            outputs="reduced_df",
+            name="reduce_columns",
+        ),
+        node(
+            func=drop_whitespace,
+            inputs="reduced_df",
+            outputs="merged_2022",
+            name="drop_whitespace",
+        ),
+
     ])
