672791c20b2355e33b890966a146531d21fa38cd
Elias Berger <elias.berger@kertos.io>
2023-04-11 09:57:39 +0200

    stemmer + stop word removal

--- src/ticketclassification/pipelines/preprocessing/nodes.py
+++ src/ticketclassification/pipelines/preprocessing/nodes.py
@@ -2,12 +2,46 @@
 This is a boilerplate pipeline 'preprocessing'
 generated using Kedro 0.18.7
 """
-import pandas as pd
-import numpy as np
-from kedro.framework.cli import catalog
-from nltk.stem import PorterStemmer, Cistem
-stemmer = Cistem()
+import re
 
+import argostranslate.package
+import argostranslate.translate
+import langdetect
+import nltk
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem import Cistem
+from nltk.tokenize import word_tokenize
+
+stemmer = Cistem()
+nltk.download('punkt')
+
+def filter_first_message_per_ticket(contents: pd.DataFrame) -> pd.DataFrame:
+    """
+    Convert the date and time column to a timestamp
+    :param contents:
+    :return:
+    """
+    # merge into timestamp
+    contents['timestamp'] = pd.to_datetime(contents['Datum'] + ' ' + contents['Uhrzeit'])
+    # filter only tickets with a description
+    contents = contents[contents["Nachrichtentyp"].str.contains("Beschreibung")]
+    # filter for initial messages only
+    initial_timestamps = contents.groupby('ID')['timestamp'].min()
+    initial_messages = pd.DataFrame({'timestamp': initial_timestamps}).reset_index()
+    initial_messages = initial_messages.merge(contents, on=['ID', 'timestamp'], how='left')
+    return initial_messages
+
+
+def extract_initial_message_per_ticket(contents: pd.DataFrame) -> pd.DataFrame:
+    """
+    Extract the initial message per ticket. Per ticket ID find the first message by timestamp
+    :param contents:
+    :return:
+    """
+    contents = contents.sort_values(by=["ID", "Timestamp"])
+    return contents
 
 def merge_tickets_with_contents(tickets: pd.DataFrame, contents: pd.DataFrame) -> pd.DataFrame:
     return pd.merge(contents, tickets, on="ID", how="inner")
@@ -49,3 +83,31 @@ def porter_stemmer(tickets: pd.DataFrame):
     return tickets
 
 
+def translate_to_english(merged_df: pd.DataFrame):
+    from_code = "de"
+    to_code = "en"
+
+    # Download and install Argos Translate package
+    argostranslate.package.update_package_index()
+    available_packages = argostranslate.package.get_available_packages()
+    package_to_install = next(
+        filter(
+            lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+        )
+    )
+    argostranslate.package.install_from_path(package_to_install.download())
+    merged_df["Text"] = merged_df["Text"].apply(lambda x: argostranslate.translate.translate(x, from_code, to_code) if langdetect.detect(x) == "de" else x)
+    return merged_df
+
+
+def stop_word_removal(tickets: pd.DataFrame):
+    # lower case
+    tickets["Text"] = tickets["Text"].str.lower()
+    # apply word tokenizer
+    tickets["Text"] = tickets["Text"].apply(word_tokenize)
+    # remove stop words
+    stop_words = {'but', 'their', 'our', 'she', 'any', 'has', 'own', 'themselves', 'can', 'what', 'not', 'same', 'been', 'those', 'now', 'ain', 'very', 'why', 'shouldn', 'were', 'then', 'weren', 'myself', 'while', 'about', 'no', 'here', 'above', 'both', "aren't", 'into', 'at', 'don', 'wasn', 'hers', 'off', 'more', 'between', 'its', "didn't", 'shan', 'ours', 'wouldn', "you'd", 'it', 'as', 'each', 'over', 'mightn', 'won', 'was', 'haven', 'yours', 'and', 'up', 'such', 'if', 'with', 'nor', 'in', 'of', 'by', 'i', 'there', 's', 'should', 'when', "should've", 'some', "shan't", 'herself', 'all', "won't", "shouldn't", 'them', 'down', 'further', "that'll", 'o', 'once', "couldn't", 'his', 'your', 'on', "haven't", 'to', 'didn', 'during', "you'll", 'an', 'we', 'most', 'am', 'me', "you've", 'hasn', 'itself', 'himself', 'ourselves', 'whom', 'that', 'll', 'my', 'through', 'y', 'until', "mightn't", 'couldn', 'this', 'they', 'than', 'yourself', 'he', 'before', 'her', "it's", 'from', 'him', 'are', 'which', 'just', 'you', 've', 'against', "doesn't", 'will', 'be', 'doing', "don't", 't', 'theirs', 'too', 'out', 'for', 'being', 'having', 'isn', 'below', 'm', "she's", 'd', "weren't", 'needn', 'have', "you're", 'does', 'other', 'so', 'is', 'where', 'doesn', 'hadn', 'few', "hadn't", "mustn't", 'only', 'these', 'under', 'ma', 'the', 'yourselves', "isn't", 'who', 'how', "hasn't", "wouldn't", 'do', 'because', "needn't", 'after', "wasn't", 're', 'did', 'again', 'aren', 'a', 'mustn', 'had', 'or'}
+    tickets["Text"] = tickets["Text"].apply(lambda x: [word for word in x if word not in stop_words])
+    # join the words back into a sentence
+    tickets["Text"] = tickets["Text"].apply(lambda x: ' '.join(x))
+    return tickets
--- src/ticketclassification/pipelines/preprocessing/pipeline.py
+++ src/ticketclassification/pipelines/preprocessing/pipeline.py
@@ -5,28 +5,60 @@ generated using Kedro 0.18.7
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import merge_tickets_with_contents, reduce_columns, drop_whitespace, porter_stemmer
+from .nodes import (
+    merge_tickets_with_contents,
+    reduce_columns,
+    drop_whitespace,
+    porter_stemmer,
+    filter_first_message_per_ticket,
+    translate_to_english,
+    stop_word_removal
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([
-        node(
-            func=merge_tickets_with_contents,
-            inputs=["tickets_2022", "contents_2022"],
-            outputs="merged_df",
-            name="merge_tickets_with_contents",
-        ),
-        node(
-            func=reduce_columns,
-            inputs="merged_df",
-            outputs="reduced_df",
-            name="reduce_columns",
-        ),
-        node(
-            func=drop_whitespace,
-            inputs="reduced_df",
-            outputs="merged_2022",
-            name="drop_whitespace",
-        ),
-
-    ])
+    return pipeline(
+        [
+            node(
+                func=filter_first_message_per_ticket,
+                inputs="contents_2022",
+                outputs="filtered_contents_2022",
+                name="filter_first_message_per_ticket",
+            ),
+            node(
+                func=merge_tickets_with_contents,
+                inputs=["tickets_2022", "filtered_contents_2022"],
+                outputs="merged_df",
+                name="merge_tickets_with_contents",
+            ),
+            node(
+                func=reduce_columns,
+                inputs="merged_df",
+                outputs="reduced_df",
+                name="reduce_columns",
+            ),
+            node(
+                func=drop_whitespace,
+                inputs="reduced_df",
+                outputs="clean_df",
+                name="drop_whitespace_rows",
+            ),
+            node(
+                func=translate_to_english,
+                inputs="clean_df",
+                outputs="translated_2022",
+                name="translate_to_english",
+            ),
+            node(
+                func=stop_word_removal,
+                inputs="translated_2022",
+                outputs="removed_stop_words_2022",
+                name="stop_word_removal",
+            ),
+            node(func=porter_stemmer,
+                 inputs="removed_stop_words_2022",
+                 outputs="primary_2022",
+                 name="porter_stemmer"
+            ),
+        ]
+    )
