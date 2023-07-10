475a86f1108c73f38ab62c19aa66b0fdbe4b8798
Elias Berger <elias.berger@kertos.io>
2023-07-10 13:42:12 +0200

    fastapi openai integration

--- main.py
+++ main.py
@@ -49,9 +49,30 @@ def preprocess_input(char: str) -> str:
     # limit length
     tiktoken.get_encoding("r50k_base")
     encoder = tiktoken.encoding_for_model("ada")
-    char = encoder.decode(encoder.encode(char)[: 2048 - 9 - 1])#
+    char = encoder.decode(encoder.encode(char)[: 2048 - 9 - 1])
+    char = char + "\n\n###\n\n"
     return char
+
+
+def gpt_3_classification(char: str) -> str:
+    """
+    Classifies the data using the GPT-3 API
+    :param char:
+    :return:
+    """
+    import openai
+    import os
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    model = openai.FineTune.retrieve("ft-oGEtDF48FowWEF0mUhaDskGh")
+    response = openai.Completion.create(
+        model=model.fine_tuned_model,
+        prompt=char,
+        temperature=0,
+        max_tokens=1,
+        logprobs=2,
+    )
+    return response["choices"][0]["text"]
 
 @app.post("/classify")
 def process_strings(char: str = Form(...), kategorie: str = Form(...), unterkategorie: str = Form(...)):
-    return preprocess_input(char)
+    return gpt_3_classification(preprocess_input(char)).strip()
